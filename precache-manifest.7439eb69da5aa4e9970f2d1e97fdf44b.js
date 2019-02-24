66bb9df0b02a4446b51d8d82f7e36c6652747cba
Songsterr99 <mr.songsterr@mail.ru>
2019-02-24 19:42:09 +0300

    Updates

---
+++ precache-manifest.7439eb69da5aa4e9970f2d1e97fdf44b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb7f7e3c5b30673c05a8",
+    "url": "/color-picker/static/js/runtime~main.eb7f7e3c.js"
+  },
+  {
+    "revision": "a3086eb78965793c7d17",
+    "url": "/color-picker/static/js/main.a3086eb7.chunk.js"
+  },
+  {
+    "revision": "206ce0228ac34b3483f2",
+    "url": "/color-picker/static/js/2.206ce022.chunk.js"
+  },
+  {
+    "revision": "a3086eb78965793c7d17",
+    "url": "/color-picker/static/css/main.45dbfd3b.chunk.css"
+  },
+  {
+    "revision": "b0a5313807f2fa4a43f1bef3612c50f7",
+    "url": "/color-picker/index.html"
+  }
+];
